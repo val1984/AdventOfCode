7de01c7d3b4ea1a1a1344c8ec95a17a2deb76ca2
Valentin Descamps <val1984@gmail.com>
2023-12-04 13:55:59 +0100

    Add day 4

---
+++ 2023/04.ts
@@ -0,0 +1,48 @@
+import { day04Inputs } from "./04data";
+
+function computePoints(cards: string[]) {
+  return cards.map((line) => {
+    const [winningCards, drawCards] = line.split(" | ");
+    const winning = winningCards.trim().split(/\s+/);
+    const draws = drawCards.trim().split(/\s+/);
+    const { length: successDraws } = draws.filter((draw) =>
+      winning.includes(draw)
+    );
+    return successDraws;
+  });
+}
+
+function part1(cards: string[]) {
+  const successes = computePoints(cards);
+  return successes.reduce(
+    (acc, successes) => acc + (successes === 0 ? 0 : 2 ** (successes - 1)),
+    0
+  );
+}
+
+console.log("Part 1", part1(day04Inputs));
+
+function part2(cards: string[]) {
+  const successes = computePoints(cards);
+  const numberOfCards: number[] = [];
+
+  successes.forEach((success, currentCardPosition) => {
+    numberOfCards[currentCardPosition] ??= 0;
+    numberOfCards[currentCardPosition]++;
+    for (
+      let pos = currentCardPosition;
+      pos < currentCardPosition + success;
+      pos++
+    ) {
+      numberOfCards[pos + 1] ??= 0;
+      numberOfCards[pos + 1] += numberOfCards[currentCardPosition];
+    }
+  });
+
+  return successes.reduce(
+    (acc, _success, count) => acc + numberOfCards[count],
+    0
+  );
+}
+
+console.log('Part 2', part2(day04Inputs));
