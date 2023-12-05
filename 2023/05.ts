83b628812aac7415fa4ddc2e04b955dd3e07fd72
Valentin Descamps <valentin.descamps@sgcib.com>
2023-12-05 13:54:39 +0100

    Add part 1 of day 5

--- 2023/05.ts
+++ 2023/05.ts
@@ -1,8 +1,26 @@
-import { day05Demo } from "./05data";
+import { day05Input } from "./05data";
 
 function part1(input: string) {
-  const data = input.split('\n\n');
-  
+  const data = input.split("\n\n");
+  const seeds = data[0].replace("seeds: ", "").split(" ").map(Number);
+  const maps = data.slice(1).map((map) =>
+    map
+      .split("\n")
+      .slice(1)
+      .map((line) => line.split(" ").map(Number))
+  );
+  return Math.min(
+    ...seeds.map((seed) => {
+      return maps.reduce((prev, map) => {
+        for (const [dest, src, length] of map) {
+          if (prev >= src && prev < src + length) {
+            return prev - src + dest;
+          }
+        }
+        return prev;
+      }, seed);
+    })
+  );
 }
 
-console.log(part1(day05Demo));
+console.log("Part 1", part1(day05Input));
