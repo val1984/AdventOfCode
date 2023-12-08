1e5bf0943b1d7ed57e4c24e87d0dff8e83916f1f
Valentin Descamps <val1984@gmail.com>
2023-12-08 15:56:08 +0100

    Add day 8 part 2

--- 2023/08/08.ts
+++ 2023/08/08.ts
@@ -3,7 +3,7 @@ import { day08Input } from "./08data";
 const linePatternRegex = /(.{3}) \= \((.{3}), (.{3})\)$/;
 function readTree(flatTree: string) {
   const tree: Record<string, string[]> = {};
-  flatTree.split("\n").forEach(line => {
+  flatTree.split("\n").forEach((line) => {
     const [, origin, ...destinations] = linePatternRegex.exec(line)!;
     tree[origin] = destinations;
   });
@@ -23,3 +23,78 @@ function part1(input: string) {
 }
 
 console.log(part1(day08Input));
+
+function isPrime(primes: number[], value: number) {
+  const maxDivider = Math.sqrt(value);
+  for (const prime of primes) {
+    if (prime > maxDivider) {
+      return true;
+    } else if (value % prime === 0) {
+      return false;
+    }
+  }
+}
+
+function* primeGenerator() {
+  const primes = [2];
+  yield 2;
+  let nextCandidate = 3;
+  while (true) {
+    yield nextCandidate;
+    primes.push(nextCandidate);
+    nextCandidate += 2;
+    while (!isPrime(primes, nextCandidate)) {
+      nextCandidate += 2;
+    }
+  }
+}
+
+function computePrimeFactors(value: number) {
+  let remainder = value;
+  let primes: Record<number, number> = {};
+  let primeGen = primeGenerator();
+  while (remainder !== 1) {
+    const prime = primeGen.next().value!;
+    while (remainder % prime === 0) {
+      remainder = remainder / prime;
+      primes[prime] ??= 0;
+      primes[prime]++;
+    }
+  }
+  return primes;
+}
+
+function computeLcm(numbers: number[]) {
+  const commonFactors = numbers.map(computePrimeFactors).reduce(
+    (acc: Record<string, number>, primes) => {
+      Object.entries(primes).forEach(([prime, count]) => {
+        acc[prime] = Math.max(acc[prime] ?? 0, count);
+      });
+      return acc;
+    },
+    {}
+  );
+  return Object.entries(commonFactors).reduce(
+    (product, [prime, count]) => product * Number(prime) ** count,
+    1
+  );
+}
+
+function part2(input: string) {
+  const [instructions, flatTree] = input.split("\n\n");
+  const tree = readTree(flatTree);
+  let startingPoints = Object.keys(tree).filter((step) => step.endsWith("A"));
+  const cycleLengths = startingPoints.flatMap((startingPoint) => {
+    let currentStep = startingPoint;
+    let count = 0;
+    while (!currentStep.endsWith("Z")) {
+      const instruction = instructions[count++ % instructions.length];
+      const newStep = tree[currentStep][instruction === "L" ? 0 : 1];
+      currentStep = newStep;
+    }
+    return count;
+  });
+  return computeLcm(cycleLengths);
+}
+
+console.log(part2(day08Input));
