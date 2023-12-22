fcefcc1deb428aa3bacf53b1106166f7b70ca257
Valentin Descamps <valentin.descamps@sgcib.com>
2023-12-22 17:01:35 +0100

    Day 22 try to construct the world

--- 2023/22/22.ts
+++ 2023/22/22.ts
@@ -1,5 +1,5 @@
 import { day22Demo, day22Input } from "./22data.ts";
-import assert from "assert";
+import assert from "node:assert";
 
 type Coords = [x: number, y: number, z: number];
 interface Brick {
@@ -9,33 +9,47 @@ interface Brick {
   supports: Brick[];
 }
 
+const X = 0;
+const Y = 1;
+const Z = 2;
+
 function makeThemFall(sortedBricks: [Coords, Coords][]) {
   const width = Math.max(...sortedBricks.map(([, [x]]) => x)) + 1;
   const depth = Math.max(...sortedBricks.map(([, [, y]]) => y)) + 1;
   const height = Math.max(...sortedBricks.map(([, [, , z]]) => z)) + 1;
-  const levels = Array.from(Array(width), () =>
-    Array.from(Array(depth), () => 0)
+  const world: (Brick | undefined)[][][] = Array.from(Array(width), () =>
+    Array.from(Array(depth), () => Array.from(Array(height), () => undefined))
   );
-  console.log({ width, depth, height, levels });
-  const bricksAtLevel: [Coords, Coords][][] = [];
   for (const [start, end] of sortedBricks) {
-    let newZ = levels[start[0]][start[1]];
-    for (let x = start[0]; x <= end[0]; x++) {
-      for (let y = start[1]; y <= end[1]; y++) {
-        newZ = levels[x][y] > newZ ? levels[x][y] : newZ;
+    let newZ = 0;
+    const supports = [];
+    for (let x = start[X]; x <= end[X]; x++) {
+      for (let y = start[Y]; y <= end[Y]; y++) {
+        for (let z = start[Z]; z > 0; z--) {
+          if (world[x][y][z] !== undefined) {
+            newZ = Math.max(z, newZ);
+          }
+        }
       }
     }
-    end[2] = end[2] - start[2] + newZ + 1;
-    start[2] = newZ + 1;
-    bricksAtLevel[newZ + 1] ??= [];
-    bricksAtLevel[newZ + 1].push([start, end]);
-    for (let x = start[0]; x <= end[0]; x++) {
-      for (let y = start[1]; y <= end[1]; y++) {
-        levels[x][y] = end[2];
+    end[Z] = end[Z] - start[Z] + newZ + 1;
+    start[Z] = newZ + 1;
+
+    const brick = {
+      topLeft: start,
+      bottomRight: end,
+      supportedBy: [],
+      supports: [],
+    };
+    for (let x = start[X]; x <= end[X]; x++) {
+      for (let y = start[Y]; y <= end[Y]; y++) {
+        for (let z = start[Z]; z <= end[Z]; z++) {
+          world[x][y][z] = brick;
+        }
       }
     }
   }
-  return bricksAtLevel;
+  return world;
 }
 
 function checkOverlap(
@@ -62,24 +76,9 @@ function part1(input: string) {
     assert(z1 <= z2);
   });
   bricks.sort(([[, , z1a]], [[, , z2a]]) => z1a - z2a);
-  const bricksByLevel = makeThemFall(bricks);
-  const linkedBricks: Brick[] = [];
-  let total = 0;
-  for (let i = 1; i < bricksByLevel.length; i++) {
-    const level = bricksByLevel[i];
-    for (const brick of level) {
-      let supports = 0;
-      const [[, , z]] = brick;
-      if (z+1 < bricksByLevel.length) {
-        for (const brickAbove of bricksByLevel[z + 1]) {
-          if (checkOverlap(brick, brickAbove)) {
-            supports++;
-          }
-        }
-      }
-    }
-  }
-  return bricksByLevel;
+  const world = makeThemFall(bricks);
+
+  return world;
 }
 
 console.log("Part 1", part1(day22Demo));
