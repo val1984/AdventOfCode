47bfd0e57ae6d2bda61674be3a21151ef028407d
Valentin Descamps <val1984@gmail.com>
2023-12-22 16:02:32 +0100

    More day 22 impl

--- 2023/22/22.ts
+++ 2023/22/22.ts
@@ -1,23 +1,51 @@
 import { day22Demo, day22Input } from "./22data.ts";
+import assert from "assert";
 
 type Coords = [x: number, y: number, z: number];
+interface Brick {
+  topLeft: Coords;
+  bottomRight: Coords;
+  supportedBy: Brick[];
+  supports: Brick[];
+}
 
-function makeThemFall(bricks: [Coords, Coords][]) {
-  const bricksFromGround = bricks.toSorted(
-    ([[, , z1a], [, , z1b]], [[, , z2a], [, , z2b]]) =>
-      Math.min(z1a, z1b) - Math.min(z2a, z2b)
-  );
-  const width = Math.max(...bricks.flatMap(([[x1], [x2]]) => [x1, x2])) + 1;
-  const depth = Math.max(...bricks.flatMap(([[, y1], [, y2]]) => [y1, y2])) + 1;
-  const height =
-    Math.max(...bricks.flatMap(([[, , z1], [, , z2]]) => [z1, z2])) + 1;
+function makeThemFall(sortedBricks: [Coords, Coords][]) {
+  const width = Math.max(...sortedBricks.map(([, [x]]) => x)) + 1;
+  const depth = Math.max(...sortedBricks.map(([, [, y]]) => y)) + 1;
+  const height = Math.max(...sortedBricks.map(([, [, , z]]) => z)) + 1;
   const levels = Array.from(Array(width), () =>
     Array.from(Array(depth), () => 0)
   );
   console.log({ width, depth, height, levels });
-  for (const [start, end] of bricks) {
+  const bricksAtLevel: [Coords, Coords][][] = [];
+  for (const [start, end] of sortedBricks) {
+    let newZ = levels[start[0]][start[1]];
+    for (let x = start[0]; x <= end[0]; x++) {
+      for (let y = start[1]; y <= end[1]; y++) {
+        newZ = levels[x][y] > newZ ? levels[x][y] : newZ;
+      }
+    }
+    end[2] = end[2] - start[2] + newZ + 1;
+    start[2] = newZ + 1;
+    bricksAtLevel[newZ + 1] ??= [];
+    bricksAtLevel[newZ + 1].push([start, end]);
+    for (let x = start[0]; x <= end[0]; x++) {
+      for (let y = start[1]; y <= end[1]; y++) {
+        levels[x][y] = end[2];
+      }
+    }
   }
-  return bricksFromGround;
+  return bricksAtLevel;
+}
+
+function checkOverlap(
+  [[x1a, y1a], [x2a, y2a]]: [Coords, Coords],
+  [[x1b, y1b], [x2b, y2b]]: [Coords, Coords]
+) {
+  return (
+    Math.max(x1a, x1b) < Math.min(x2a, x2b) &&
+    Math.max(y1a, y1b) < Math.min(y2a, y2b)
+  );
 }
 
 function part1(input: string) {
@@ -28,7 +56,30 @@ function part1(input: string) {
       Coords
     ];
   });
-  return makeThemFall(bricks);
+  bricks.forEach(([[x1, y1, z1], [x2, y2, z2]]) => {
+    assert(x1 <= x2);
+    assert(y1 <= y2);
+    assert(z1 <= z2);
+  });
+  bricks.sort(([[, , z1a]], [[, , z2a]]) => z1a - z2a);
+  const bricksByLevel = makeThemFall(bricks);
+  const linkedBricks: Brick[] = [];
+  let total = 0;
+  for (let i = 1; i < bricksByLevel.length; i++) {
+    const level = bricksByLevel[i];
+    for (const brick of level) {
+      let supports = 0;
+      const [[, , z]] = brick;
+      if (z+1 < bricksByLevel.length) {
+        for (const brickAbove of bricksByLevel[z + 1]) {
+          if (checkOverlap(brick, brickAbove)) {
+            supports++;
+          }
+        }
+      }
+    }
+  }
+  return bricksByLevel;
 }
 
-console.log("Part 1", part1(day22Input));
+console.log("Part 1", part1(day22Demo));
