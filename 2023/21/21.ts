e45a586d4cdcb5245c5f58e3832e53e880fc26ee
Valentin Descamps <val1984@gmail.com>
2023-12-26 21:15:04 +0100

    Add day 21 part 2

--- 2023/21/21.ts
+++ 2023/21/21.ts
@@ -1,4 +1,6 @@
+import assert from "node:assert";
 import { day21Demo, day21Input } from "./21data.ts";
+import { transpose } from "../../utils/matrix.ts";
 
 type Coords = [r: number, c: number];
 
@@ -17,10 +19,6 @@ function coordsToNumber([r, c]: Coords) {
   return r * 1_000 + c;
 }
 
-function numberToCoords(value: number): Coords {
-  return [Math.floor(value / 1_000), value % 1_000];
-}
-
 const neighbors: Coords[] = [
   [0, 1],
   [1, 0],
@@ -28,12 +26,18 @@ const neighbors: Coords[] = [
   [-1, 0],
 ];
 
-function part1(input: string, iterations: number) {
-  const rows = input.split("\n");
-  const start = findStartCoords(rows);
+function fill(rows: string[], start: Coords): [number, number, number];
+function fill(rows: string[], start: Coords, iterations: number): number;
+function fill(
+  rows: string[],
+  start: Coords,
+  iterations?: number
+): [number, number, number] | number {
   let waveSize = 1;
+  let prevWaveSize = 1;
   let prevWave: Coords[] = [start];
-  for (let i = 0; i < iterations; i++) {
+  const loopLimit = iterations ?? Number.MAX_SAFE_INTEGER;
+  for (let i = 1; i <= loopLimit; i++) {
     const visited = new Set<number>();
     // console.log("Wave", i);
     const currentWave: Coords[] = [];
@@ -56,16 +60,84 @@ function part1(input: string, iterations: number) {
         }
       }
     }
+    if (!iterations && prevWaveSize === visited.size) {
+      console.log("Cycle detected at", i);
+      return [i - 2, prevWaveSize, waveSize];
+    }
+    prevWaveSize = waveSize;
     waveSize = visited.size;
     prevWave = currentWave;
+    // console.log("Wave", i, waveSize);
   }
+  return waveSize;
+}
+
+function part1(input: string, iterations: number) {
+  const rows = input.split("\n");
+  const start = findStartCoords(rows);
+  let waveSize = fill(rows, start, iterations);
   return waveSize;
 }
 
 console.log("Part 1", part1(day21Input, 64));
 
-// function part2(input: string) {
-//   return input;
-// }
+function part2(input: string, iterations: number) {
+  const rows = input.split("\n");
+  const size = rows.length;
+  assert(size === rows[0].length);
+  const start = findStartCoords(rows);
+  assert(Math.floor(size / 2) === start[0]);
+  assert(start[0] === start[1]);
+  const middle = start[0];
+  assert(
+    Array.from(rows[middle]).every((char) => char === "." || char === "S")
+  );
+  assert(
+    Array.from(transpose(rows)[middle]).every(
+      (char) => char === "." || char === "S"
+    )
+  );
+  const even = fill(rows, start, 2 * size);
+  const odd = fill(rows, start, 2 * size + 1);
+  const gridsCount = (iterations - middle) / rows.length;
+  console.log(gridsCount);
+  const cornerStarts: Coords[] = [
+    [middle, size - 1],
+    [size - 1, middle],
+    [middle, 0],
+    [0, middle],
+  ];
+  const corners = cornerStarts.reduce(
+    (cornersCount, coords) => cornersCount + fill(rows, coords, size - 1),
+    0
+  );
 
-// console.log('Part 2', part2(day21Demo));
+  const sideStarts: Coords[] = [
+    [size - 1, size - 1],
+    [0, size - 1],
+    [0, 0],
+    [size - 1, 0],
+  ];
+  const small = sideStarts.reduce(
+    (cornersCount, coords) => cornersCount + fill(rows, coords, middle - 1),
+    0
+  );
+  const large = sideStarts.reduce(
+    (cornersCount, coords) =>
+      cornersCount + fill(rows, coords, Math.floor((3 * size) / 2) - 1),
+    0
+  );
+  let total = 0;
+  const mod = gridsCount % 2;
+  for (let r = 1; r < gridsCount; r++) {
+    const evenPerRow = r - mod;
+    const oddPerRow = r - 1 + mod;
+    total += 2 * evenPerRow * even + 2 * oddPerRow * odd;
+  }
+  total += gridsCount * even + (gridsCount - 1) * odd;
+  total += corners + gridsCount * small + (gridsCount - 1) * large;
+
+  return total;
+}
+
+console.log("Part 2", part2(day21Input, 26501365));
