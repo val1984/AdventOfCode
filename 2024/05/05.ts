1c8dc2a1bb9872e63e5f3d33f40be467f6eb6626
Valentin Descamps <val1984@gmail.com>
2024-12-05 06:56:54 +0100

    Add day 5 2024 part 2

--- 2024/05/05.ts
+++ 2024/05/05.ts
@@ -37,3 +37,36 @@ function part1(inputs: string) {
 
 console.log(part1(day05Inputs));
 
+function makeSorter(rules: Record<number, number[]>) {
+  return function sorter(x: number, y: number) {
+    const rule = rules[x];
+    if (rule?.includes(y)) {
+      return -1;
+    }
+    return 0;
+  };
+}
+
+function part2(inputs: string) {
+  const [rules, manuals] = parse(inputs);
+  const sorter = makeSorter(rules);
+  let middlePageTotal = 0;
+  for (const manual of manuals) {
+    const visitedPages: number[] = [];
+    let isIncorrect = false;
+    for (const page of manual) {
+      const hasViolatedRule = rules[page]?.some((rule) =>
+        visitedPages.some((prev) => prev === rule)
+      );
+      isIncorrect = hasViolatedRule || isIncorrect;
+      visitedPages.push(page);
+    }
+    if (isIncorrect) {
+      const sorted = manual.sort(sorter);
+      middlePageTotal += sorted[Math.floor(sorted.length / 2)];
+    }
+  }
+  return middlePageTotal;
+}
+
+console.log(part2(day05Inputs));
